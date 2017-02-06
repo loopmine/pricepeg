856c9cff32f0560a3f2dea689c53d0b23f935359
Dan Wasyluk <dwasyluk@gmail.com>
2017-02-05 22:38:24 -0500

    DIST cleanup

--- dist/doc-utils/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var DocUtils_1 = require("./DocUtils");
-var common_1 = require("../common");
-var markdown = DocUtils_1.supportedCurrenciesToMarkdown(common_1.supportedCurrencies);
-console.log("Supported Currency Markdown:\n             " + markdown);
-//# sourceMappingURL=index.js.map
